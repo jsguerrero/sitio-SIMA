486190efe772a5d4c0611e4d0dd8df9784698279
Sebastián Guerrero <sebas.dobleguerrero@gmail.com>
2020-07-11 16:58:33 -0500

    Django
    Aplicacion para gestión del sitio.
    
    - index
    - menú
    - login
    - logout

---
+++ datascience/app/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import redirect
+
+# SI ACCEDE A LA PAGINA PRINCIPAL
+# Y NO HA INICIADO SESION
+# SE REDIRIGE A LA PAGINA DE INICIO DE SESION
+# SI YA HA INICIADO SESION
+# SE REDIRIGE A LA PAGINA DE INICIO
+def index(request):
+    if request.user.is_authenticated:
+        return render(request,'index.html')
+    else:
+        return render(request,'login.html')
+
+# UNA VEZ QUE SE INCIA SESION
+# SE REDIRIGE A LA PAGINA DE INICIO
+# SI NO SE HA PODIDO INICIAR SESION
+# SE IMPRIME MENSAJE DE ERROR
+from django.contrib.auth import authenticate, login as dj_login
+from django.contrib import messages
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            dj_login(request, user)
+            next_url = request.POST['next']
+            print(next_url)
+            if not next_url:
+                next_url = 'app:index'
+            return redirect(next_url)
+        else:
+            messages.error(request,'Datos incorrectos')
+            return redirect('app:login')
+    else:
+        return render(request,'login.html')
+
+from django.contrib.auth import logout as dj_logout
+def logout(request):
+    dj_logout(request)
+    return redirect('app:index')
--- datascience/datascience/urls.py
+++ datascience/datascience/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('app/', include('app.urls')),
+]
+
+# SE AGREGA LA APLICACION DESDE EL ACCESO RAIZ
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/app/', permanent=True)),
 ]
