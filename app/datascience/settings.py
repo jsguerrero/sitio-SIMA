64d570a1b947104bc81c21ff6fd012fb04d53dc6
Sebastián Guerrero <sebas.dobleguerrero@gmail.com>
2020-10-12 18:15:48 -0500

    Entrega proyecto.
    Se dearrollaron todas las funcionalidades que se presentan en el reporte del proyecto.

--- app/datascience/settings.py
+++ app/datascience/settings.py
@@ -108,12 +108,11 @@ ASGI_APPLICATION = 'datascience.routing.application'
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
-        "NAME": os.environ.get("POSTGRES_DB", os.path.join(BASE_DIR, "db.sqlite3")),
+        "NAME": os.environ.get("POSTGRES_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("POSTGRES_USER", "user"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "password"),
         "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
-        "CONN_MAX_AGE": 500
+        "PORT": os.environ.get("POSTGRES_PORT", "5432")
     }
 }
 
